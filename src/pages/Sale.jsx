dc09ef841b0dd01830e6262007fe44fdf3b16fe5
nicholascostap <nickxxtreme@gmail.com>
2025-06-02 02:58:10 -0300

    :bug: fix(Sale): import para estilização

--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import "../styles/sale.css";
 
 const Sale = () => {
     const navigate = useNavigate();
